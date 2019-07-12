2ffe09d6b2297cfa9545643b45da3ef66519a231
JKnight54321 <42787771+JKnight54321@users.noreply.github.com>
2019-07-12 14:23:43 -0600

    Created empty pages      -Alerts      -Assemble      -Inventory      -Orders      -Reports      -Vendor Added commented out [Authorize] tags to all related cshtml.cs files
    Updated Index.cshtml to point to the new pages

--- Jabar/Pages/Inventory.cshtml.cs
+++ Jabar/Pages/Inventory.cshtml.cs
@@ -1,19 +1,17 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Jabar.Pages
 {
     // [Authorize] // Uncomment this when we are ready to implement Identity
-    public class IndexModel : PageModel
+    public class InventoryModel : PageModel
     {
         public void OnGet()
         {
-
         }
     }
 }

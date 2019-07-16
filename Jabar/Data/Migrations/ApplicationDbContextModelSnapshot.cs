c65145d1fe594a12100c5d7f5ce3a16c34cbdeda
Aaron Mortensen <aaronmortensen@mail.weber.edu>
2019-07-16 12:46:13 -0600

    fixed some db stuff... I hope

--- Jabar/Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Jabar/Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -4,16 +4,14 @@ using Jabar.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Jabar.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20190712032449_initialsetup")]
-    partial class initialsetup
+    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
     {
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

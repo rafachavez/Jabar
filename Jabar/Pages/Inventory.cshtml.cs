7030226cbaa1691e07a6770f9455a7ae3bc1103f
Aaron Mortensen <aaronmortensen@mail.weber.edu>
2019-07-18 16:56:58 -0600

    add and subtract qty buttons work... they arent pretty but they work!

--- Jabar/Pages/Inventory.cshtml.cs
+++ Jabar/Pages/Inventory.cshtml.cs
@@ -19,11 +19,103 @@ namespace Jabar.Pages
             _context = context;
         }
 
-        public IList<Item> Item { get; set; }
+        public IList<Item> Items { get; set; }
+        [BindProperty]
+        public Item Item { get; set; }
+        //[BindProperty]
+        //public int Index { get; set; }
 
         public async Task OnGetAsync()
         {
-            Item = await _context.Items.ToListAsync();
+            Items = await _context.Items.ToListAsync();
         }
+
+        //////////////////////////////////////////////
+
+
+        public async Task<IActionResult> OnPostAddAsync(int id)
+        {
+            Items = await _context.Items.ToListAsync();
+            Item = Items[id-1];
+            Item.OnHandQty++;
+            _context.Attach(Item).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ItemExists(Item.ItemId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostSubtractAsync(int id)
+        {
+            Items = await _context.Items.ToListAsync();
+            Item = Items[id - 1];
+            Item.OnHandQty--;
+            _context.Attach(Item).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ItemExists(Item.ItemId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int index)
+        {
+            Item = Items[index];
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Item).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ItemExists(Item.ItemId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool ItemExists(int id)
+        {
+            return _context.Items.Any(e => e.ItemId == id);
+        }
+
     }
 }

d1e3b0a4bedf36ebc35b90ff1a69a867aacb959f
Aaron Mortensen <aaronmortensen@mail.weber.edu>
2019-07-18 21:59:50 -0600

    commented the code so its easy to replicate and modify

--- Jabar/Pages/Inventory.cshtml.cs
+++ Jabar/Pages/Inventory.cshtml.cs
@@ -58,15 +58,27 @@ namespace Jabar.Pages
             return RedirectToPage();
         }
 
+        /// <summary>
+        /// This is the method called by asp-page-handler="Subtract"
+        /// it gets id from asp-route-id="some int"
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public async Task<IActionResult> OnPostSubtractAsync(int id)
         {
+            //got to get all the items.... maybe a better developer doesnt but I did
             Items = await _context.Items.ToListAsync();
+            //get the exact item that is being interacted with
+            //the id passed in isnt 0 based so you have to subtract 1
             Item = Items[id - 1];
+            //subtract one from it
             Item.OnHandQty--;
             if(Item.OnHandQty < 0)
             {
-                Item.OnHandQty = 0;
+                Item.OnHandQty = 0;//no negative inventory
             }
+            //get db stuff, I copied the rest of this code from the scaffolding
+            //so I can only follow it, not really create it yet
             _context.Attach(Item).State = EntityState.Modified;
 
             try
@@ -84,37 +96,10 @@ namespace Jabar.Pages
                     throw;
                 }
             }
+            //stay on current page
             return RedirectToPage();
         }
-
-        public async Task<IActionResult> OnPostAsync(int index)
-        {
-            Item = Items[index];
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-
-            _context.Attach(Item).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ItemExists(Item.ItemId))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return RedirectToPage("./Index");
-        }
+       
 
         private bool ItemExists(int id)
         {

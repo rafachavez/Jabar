a4087286c698266d503dee7593ef87b6e5fa865c
JKnight54321 <42787771+JKnight54321@users.noreply.github.com>
2019-07-27 09:04:35 -0600

    Updated Vendors Folder      -Replaced Vendor page with Vendors/Index      -Added Crud pages (Delete, Details, Edit)      -Updated Crud page Create Deleted Original Vendor Page

---
+++ Jabar/Pages/Vendors/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Jabar.Data;
+using Jabar.Models;
+
+namespace Jabar.Pages.Vendors
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Jabar.Data.ApplicationDbContext _context;
+
+        public DeleteModel(Jabar.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Vendor Vendor { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Vendor = await _context.Vendors.FirstOrDefaultAsync(m => m.VendorId == id);
+
+            if (Vendor == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Vendor = await _context.Vendors.FindAsync(id);
+
+            if (Vendor != null)
+            {
+                _context.Vendors.Remove(Vendor);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Jabar/Pages/Vendors/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Jabar.Data;
+using Jabar.Models;
+
+namespace Jabar.Pages.Vendors
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Jabar.Data.ApplicationDbContext _context;
+
+        public DetailsModel(Jabar.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Vendor Vendor { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Vendor = await _context.Vendors.FirstOrDefaultAsync(m => m.VendorId == id);
+
+            if (Vendor == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}

d27cf9cce805eda7942dc1dcc2183a8adc7f4d97
rafaeljchavez <42992298+rafaeljchavez@users.noreply.github.com>
2019-07-29 01:29:20 -0600

    changed the default object to the new Application User and made sure other fields are saved to the db

--- Jabar/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Jabar/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using Jabar.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
@@ -79,7 +80,7 @@ namespace Jabar.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email, name = Input.Name, PhoneNumber=Input.PhoneNumber };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {

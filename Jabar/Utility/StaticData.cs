95706cd16283716477aeb7aedc86cc7074554093
rafaeljchavez <42992298+rafaeljchavez@users.noreply.github.com>
2019-07-29 01:38:32 -0600

    created static class to store admin users

---
+++ Jabar/Utility/StaticData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jabar.Utility
+{
+    public class StaticData
+    {
+        public const string AdminEndUser = "Admin";
+        public const string SuperAdminEndUser = "Super Admin";
+    }
+}
